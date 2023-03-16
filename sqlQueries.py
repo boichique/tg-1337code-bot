bf091bd521628af02f321355072c9b4ce8e3a2d8
boichique <iuvarov97@gmail.com>
2023-03-16 17:32:37 +0300

    Исправил sql запросы

--- sqlQueries.py
+++ sqlQueries.py
@@ -5,28 +5,25 @@ stat = """SELECT name,
         FROM tgChallengeBot.reports AS t1
         GROUP BY name;"""
 
-today = """SELECT name, 
-        IFNULL((SELECT COUNT(id) FROM tgChallengeBot.reports AS t2 WHERE t1.name = t2.name AND level like "easy" GROUP BY id), 0) AS easy,
-        IFNULL((SELECT COUNT(id) FROM tgChallengeBot.reports AS t2 WHERE t1.name = t2.name AND level like "medium" GROUP BY id), 0) AS medium,
-        IFNULL((SELECT COUNT(id) FROM tgChallengeBot.reports AS t2 WHERE t1.name = t2.name AND level like "hard" GROUP BY id), 0) AS hard
+today = """SELECT name,
+        IFNULL((SELECT COUNT(id) FROM tgChallengeBot.reports AS t2 WHERE t1.name = t2.name AND level like "easy" AND DATE(date) = CURDATE() GROUP BY id), 0) AS easy,
+        IFNULL((SELECT COUNT(id) FROM tgChallengeBot.reports AS t2 WHERE t1.name = t2.name AND level like "medium" AND DATE(date) = CURDATE() GROUP BY id), 0) AS medium,
+        IFNULL((SELECT COUNT(id) FROM tgChallengeBot.reports AS t2 WHERE t1.name = t2.name AND level like "hard" AND DATE(date) = CURDATE() GROUP BY id), 0) AS hard
         FROM tgChallengeBot.reports AS t1
-        WHERE DATE(date) = CURDATE() 
-        GROUP BY name;"""
+        GROUP BY name"""
 
 yesterday = """SELECT name, 
-        IFNULL((SELECT COUNT(id) FROM tgChallengeBot.reports AS t2 WHERE t1.name = t2.name AND level like "easy" GROUP BY id), 0) AS easy,
-        IFNULL((SELECT COUNT(id) FROM tgChallengeBot.reports AS t2 WHERE t1.name = t2.name AND level like "medium" GROUP BY id), 0) AS medium,
-        IFNULL((SELECT COUNT(id) FROM tgChallengeBot.reports AS t2 WHERE t1.name = t2.name AND level like "hard" GROUP BY id), 0) AS hard
+        IFNULL((SELECT COUNT(id) FROM tgChallengeBot.reports AS t2 WHERE t1.name = t2.name AND level like "easy" AND DATE(date) = DATE_SUB(CURDATE(), INTERVAL 1 DAY) GROUP BY id), 0) AS easy,
+        IFNULL((SELECT COUNT(id) FROM tgChallengeBot.reports AS t2 WHERE t1.name = t2.name AND level like "medium" AND DATE(date) = DATE_SUB(CURDATE(), INTERVAL 1 DAY) GROUP BY id), 0) AS medium,
+        IFNULL((SELECT COUNT(id) FROM tgChallengeBot.reports AS t2 WHERE t1.name = t2.name AND level like "hard" AND DATE(date) = DATE_SUB(CURDATE(), INTERVAL 1 DAY) GROUP BY id), 0) AS hard
         FROM tgChallengeBot.reports AS t1
-        WHERE DATE(date) = DATE_SUB(CURDATE(), INTERVAL 1 DAY)
         GROUP BY name;"""
 
 week = """SELECT name, 
-        IFNULL((SELECT COUNT(id) FROM tgChallengeBot.reports AS t2 WHERE t1.name = t2.name AND level like "easy" GROUP BY id), 0) AS easy,
-        IFNULL((SELECT COUNT(id) FROM tgChallengeBot.reports AS t2 WHERE t1.name = t2.name AND level like "medium" GROUP BY id), 0) AS medium,
-        IFNULL((SELECT COUNT(id) FROM tgChallengeBot.reports AS t2 WHERE t1.name = t2.name AND level like "hard" GROUP BY id), 0) AS hard
+        IFNULL((SELECT COUNT(id) FROM tgChallengeBot.reports AS t2 WHERE t1.name = t2.name AND level like "easy" AND DATE(date) BETWEEN DATE_SUB(NOW(), INTERVAL 1 WEEK) AND NOW() GROUP BY id), 0) AS easy,
+        IFNULL((SELECT COUNT(id) FROM tgChallengeBot.reports AS t2 WHERE t1.name = t2.name AND level like "medium" AND DATE(date) BETWEEN DATE_SUB(NOW(), INTERVAL 1 WEEK) AND NOW() GROUP BY id), 0) AS medium,
+        IFNULL((SELECT COUNT(id) FROM tgChallengeBot.reports AS t2 WHERE t1.name = t2.name AND level like "hard" AND DATE(date) BETWEEN DATE_SUB(NOW(), INTERVAL 1 WEEK) AND NOW() GROUP BY id), 0) AS hard
         FROM tgChallengeBot.reports AS t1
-        WHERE DATE(date) BETWEEN DATE_SUB(NOW(), INTERVAL 1 WEEK) AND NOW()
         GROUP BY name;"""
 
 insert = """INSERT INTO reports (`id`, `name`, `date`, `level`, `link`, `description`) 
