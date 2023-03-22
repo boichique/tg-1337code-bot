352f9b516b707b55aa02c2609d5afde89ec72ab2
boichique <iuvarov97@gmail.com>
2023-03-23 00:08:38 +0300

    Сделал корректировку под часовой пояс для SQL-запросов

--- queries.py
+++ queries.py
@@ -7,25 +7,25 @@ stat = """SELECT name,
 
 
 today = """SELECT name,
-        IFNULL((SELECT COUNT(id) FROM tgChallengeBot.reports AS t2 WHERE t1.name = t2.name AND level like "easy" AND DATE(date) = CURDATE() GROUP BY id), 0) AS easy,
-        IFNULL((SELECT COUNT(id) FROM tgChallengeBot.reports AS t2 WHERE t1.name = t2.name AND level like "medium" AND DATE(date) = CURDATE() GROUP BY id), 0) AS medium,
-        IFNULL((SELECT COUNT(id) FROM tgChallengeBot.reports AS t2 WHERE t1.name = t2.name AND level like "hard" AND DATE(date) = CURDATE() GROUP BY id), 0) AS hard
+        IFNULL((SELECT COUNT(id) FROM tgChallengeBot.reports AS t2 WHERE t1.name = t2.name AND level like "easy" AND DATE(date) = DATE(DATE_SUB(current_timestamp(), INTERVAL -3 HOUR)) GROUP BY id), 0) AS easy,
+        IFNULL((SELECT COUNT(id) FROM tgChallengeBot.reports AS t2 WHERE t1.name = t2.name AND level like "medium" AND DATE(date) = DATE(DATE_SUB(current_timestamp(), INTERVAL -3 HOUR)) GROUP BY id), 0) AS medium,
+        IFNULL((SELECT COUNT(id) FROM tgChallengeBot.reports AS t2 WHERE t1.name = t2.name AND level like "hard" AND DATE(date) = DATE(DATE_SUB(CURRENT_TIMESTAMP(), INTERVAL -3 HOUR)) GROUP BY id), 0) AS hard
         FROM tgChallengeBot.reports AS t1
-        GROUP BY name"""
+        GROUP BY name;"""
 
 
 yesterday = """SELECT name, 
-        IFNULL((SELECT COUNT(id) FROM tgChallengeBot.reports AS t2 WHERE t1.name = t2.name AND level like "easy" AND DATE(date) = DATE_SUB(CURDATE(), INTERVAL 1 DAY) GROUP BY id), 0) AS easy,
-        IFNULL((SELECT COUNT(id) FROM tgChallengeBot.reports AS t2 WHERE t1.name = t2.name AND level like "medium" AND DATE(date) = DATE_SUB(CURDATE(), INTERVAL 1 DAY) GROUP BY id), 0) AS medium,
-        IFNULL((SELECT COUNT(id) FROM tgChallengeBot.reports AS t2 WHERE t1.name = t2.name AND level like "hard" AND DATE(date) = DATE_SUB(CURDATE(), INTERVAL 1 DAY) GROUP BY id), 0) AS hard
+        IFNULL((SELECT COUNT(id) FROM tgChallengeBot.reports AS t2 WHERE t1.name = t2.name AND level like "easy" AND DATE(date) = DATE_SUB(DATE(DATE_SUB(CURRENT_TIMESTAMP(), INTERVAL -3 HOUR)), INTERVAL 1 DAY) GROUP BY id), 0) AS easy,
+        IFNULL((SELECT COUNT(id) FROM tgChallengeBot.reports AS t2 WHERE t1.name = t2.name AND level like "medium" AND DATE(date) = DATE_SUB(DATE(DATE_SUB(CURRENT_TIMESTAMP(), INTERVAL -3 HOUR)), INTERVAL 1 DAY) GROUP BY id), 0) AS medium,
+        IFNULL((SELECT COUNT(id) FROM tgChallengeBot.reports AS t2 WHERE t1.name = t2.name AND level like "hard" AND DATE(date) = DATE_SUB(DATE(DATE_SUB(CURRENT_TIMESTAMP(), INTERVAL -3 HOUR)), INTERVAL 1 DAY) GROUP BY id), 0) AS hard
         FROM tgChallengeBot.reports AS t1
         GROUP BY name;"""
 
 
 week = """SELECT name, 
-        IFNULL((SELECT COUNT(id) FROM tgChallengeBot.reports AS t2 WHERE t1.name = t2.name AND level like "easy" AND DATE(date) BETWEEN DATE_SUB(NOW(), INTERVAL 1 WEEK) AND NOW() GROUP BY id), 0) AS easy,
-        IFNULL((SELECT COUNT(id) FROM tgChallengeBot.reports AS t2 WHERE t1.name = t2.name AND level like "medium" AND DATE(date) BETWEEN DATE_SUB(NOW(), INTERVAL 1 WEEK) AND NOW() GROUP BY id), 0) AS medium,
-        IFNULL((SELECT COUNT(id) FROM tgChallengeBot.reports AS t2 WHERE t1.name = t2.name AND level like "hard" AND DATE(date) BETWEEN DATE_SUB(NOW(), INTERVAL 1 WEEK) AND NOW() GROUP BY id), 0) AS hard
+        IFNULL((SELECT COUNT(id) FROM tgChallengeBot.reports AS t2 WHERE t1.name = t2.name AND level like "easy" AND DATE(date) BETWEEN DATE_SUB(DATE_SUB(CURRENT_TIMESTAMP(), INTERVAL -3 HOUR), INTERVAL 1 WEEK) AND DATE_SUB(CURRENT_TIMESTAMP(), INTERVAL -3 HOUR) GROUP BY id), 0) AS easy,
+        IFNULL((SELECT COUNT(id) FROM tgChallengeBot.reports AS t2 WHERE t1.name = t2.name AND level like "medium" AND DATE(date) BETWEEN DATE_SUB(DATE_SUB(CURRENT_TIMESTAMP(), INTERVAL -3 HOUR), INTERVAL 1 WEEK) AND DATE_SUB(CURRENT_TIMESTAMP(), INTERVAL -3 HOUR) GROUP BY id), 0) AS medium,
+        IFNULL((SELECT COUNT(id) FROM tgChallengeBot.reports AS t2 WHERE t1.name = t2.name AND level like "hard" AND DATE(date) BETWEEN DATE_SUB(DATE_SUB(CURRENT_TIMESTAMP(), INTERVAL -3 HOUR), INTERVAL 1 WEEK) AND DATE_SUB(CURRENT_TIMESTAMP(), INTERVAL -3 HOUR) GROUP BY id), 0) AS hard
         FROM tgChallengeBot.reports AS t1
         GROUP BY name;"""
 
